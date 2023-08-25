077412e49afa5ab117eaec840485ce9cf3c50bd9
Mohamed-Elmahdy <89789199+Mohamedelmahd@users.noreply.github.com>
2023-08-25 16:40:51 +0300

    sign up screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_5/widgets/custom_buttom.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -8,21 +9,131 @@ class SignupScreen extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignupScreen> {
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( title: Text('Sign Up',style: TextStyle(color: Colors.white),),
-       backgroundColor: Colors.deepPurple, toolbarHeight: 40,
+      appBar: AppBar(
+        title: const Text(
+          'Sign Up',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.deepPurple,
+        toolbarHeight: 40,
         automaticallyImplyLeading: false,
-      leading: IconButton(
-        onPressed: () {
-          Navigator.of(context).pop();
-        },
-        icon: Icon(Icons.arrow_back_ios,color: Colors.white,) ,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
       ),
-        
-    ),
-    
+      body: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/flash_logo.png',
+                  width: 400,
+                  height: 250,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: emailcontroller,
+                    decoration: const InputDecoration(labelText: 'First Name'),
+                    validator: (value) {
+                      if (value == null || value.length > 3) {
+                        return " Enter valid First Name ";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: emailcontroller,
+                    decoration: const InputDecoration(labelText: 'Last Name'),
+                    validator: (value) {
+                      if (value == null || value.length > 3) {
+                        return " Enter valid Last Name ";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: emailcontroller,
+                    decoration: const InputDecoration(labelText: 'Email'),
+                    validator: (value) {
+                      if (value == null || value.contains("@") == false) {
+                        return " Enter valid Email ";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: passwordcontroller,
+                    obscureText: true,
+                    decoration: const InputDecoration(labelText: 'Password'),
+                    validator: (value) {
+                      if (value == null || value.length < 8) {
+                        return " Enter valid Password ";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextFormField(
+                    controller: passwordcontroller,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        labelText: 'Confirmation Password'),
+                    validator: (value) {
+                      if (value == null || value.length < 8) {
+                        return " Enter valid Re-Password ";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+                const SizedBox(height: 18.0),
+                MyCustomButton(
+                  buttonLabel: "Sign Up",
+                  onTap: () async {
+                    //Firebase Code Here
+                  },
+                ),
+                const SizedBox(height: 18.0),
+                MyCustomButton(
+                  buttonLabel: "Have An Account? Sign In",
+                  onTap: () {
+                    Navigator.maybePop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
